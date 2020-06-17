55877c2c01dcdb140951257b6672bbd9e45e109d
danielgrgly <danielgrgly@gmail.com>
2020-06-17 19:17:32 +0200

    bugfix for vehicleDetails, error handling

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -9,46 +9,56 @@ const futarApi = axios.create({
 });
 
 async function nearbyVehicles(req, res) {
-  const response = await futarApi.get("/vehicles-for-location.json", {
-    params: {
-      lat: req.query.lat,
-      lon: req.query.lon,
-      latSpan: 0.008747329406197935,
-      lonSpan: 0.010232225997739874,
-      radius: 1,
-      includeReferences: true,
-    },
-  });
+  try {
+    const response = await futarApi.get("/vehicles-for-location.json", {
+      params: {
+        lat: req.query.lat,
+        lon: req.query.lon,
+        latSpan: 0.0091,
+        lonSpan: 0.0111,
+        radius: 1,
+        includeReferences: true,
+      },
+    });
 
-  const vehicles = response.data.data.list;
-  const { stops, trips, routes } = response.data.data.references;
+    const vehicles = response.data.data.list;
+    const { stops, trips, routes } = response.data.data.references;
 
-  const data = vehicles.map((vehicle) => {
-    return {
-      ...vehicle,
-      trip: trips[vehicle.tripId],
-      stop: stops[vehicle.stopId],
-      route: routes[vehicle.routeId],
-    };
-  });
+    const data = vehicles.map((vehicle) => {
+      return {
+        ...vehicle,
+        trip: trips[vehicle.tripId],
+        stop: stops[vehicle.stopId],
+        route: routes[vehicle.routeId],
+      };
+    });
 
-  res.send(data);
+    res.send(data);
+  } catch (e) {
+    console.log(e);
+    res.send("error!");
+  }
 }
 
 async function vehicleDetails(req, res) {
-  const response = await axios.get("/trip-details.json", {
-    params: {
-      //   tripId: req.params.id,
-      vehicleId: req.params.id,
-      includeReferences: true,
-    },
-  });
+  console.log("vehicleDetails");
+  console.log(req.query);
 
-  console.log(response);
+  try {
+    const response = await futarApi.get("/trip-details.json", {
+      params: {
+        tripId: req.query.id,
+        // vehicleId: req.query.id,
+        includeReferences: true,
+      },
+    });
 
-  res.send(response.data);
+    res.send(response.data);
+  } catch (e) {
+    console.log(e);
+    res.send("error!");
+  }
 }
-
 module.exports = {
   nearbyVehicles,
   vehicleDetails,
