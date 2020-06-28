8dc3b26c7ba28f0c02aaa141a9b497ed9d3a0fcf
danielgrgly <danielgrgly@gmail.com>
2020-06-28 15:49:24 +0200

    trigger rebuild on gcp

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const mongoose = require("mongoose");
 
+console.log("process.env.MONGO_URL");
+console.log(process.env.MONGO_URL);
+
 mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true });
 
 const matchSchema = mongoose.Schema({
