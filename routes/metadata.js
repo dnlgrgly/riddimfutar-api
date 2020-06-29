2826cd98705207ad5bf8ec81e1308e902d3e618b
danielgrgly <danielgrgly@gmail.com>
2020-06-29 17:39:37 +0200

    fix bounds

--- routes/metadata.js
+++ routes/metadata.js
@@ -2,10 +2,10 @@ async function metadata(req, res) {
   res.send({
     genres: [{ path: "riddim", displayName: "Riddim" }],
     message: "Udv az appban, itt a fejleszto beszel! A tavolsagmerest kellene tesztelni, ehhez mar elesben jarmure kellene szallni. *** Jeleneg az M3-mas metro vonalan es a 2-es, 3-mas, 4-es es 6-os villamosok vonalain felujitas miatt tereles van. Kerjuk figyeld a BKK-s utastajekoztatast. ***",
-    lowerLeftLatitude: 47.57564,
-    lowerLeftLongitude: 18.94222,
-    upperRightLatitude: 47.588130,
-    upperRightLongitude: 19.07639,
+    lowerLeftLatitude: 47.170852,
+    lowerLeftLongitude: 18.878917,
+    upperRightLatitude: 47.665298,
+    upperRightLongitude: 19.360043,
   });
 }
 
