e52eb029c27d64025eb5dfb70ea3406cc6c2063b
dnlgrgly <dnlgrgly@icloud.com>
2022-06-04 02:00:27 +0200

    add dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const { Router } = require("express");
 const router = new Router();
 
 const { metadata } = require("./metadata.js");
 const { fetchMusic } = require("./music.js");
 const { nearbyVehicles, vehicleDetails } = require("./vehicles.js");
+
 
 router.get("/metadata", metadata);
 
